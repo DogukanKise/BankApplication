b6d19f383b64b758f8710f16c01d4e3dd74c5be2
dogukan.kisecuklu <dogukan.kisecuklu@DOGUKANKSCKL>
2025-02-23 18:28:54 +0300

    commit 1

--- BankCreditSystem.Application/Services/Repositories/IIndividualCustomerRepository.cs
+++ BankCreditSystem.Application/Services/Repositories/IIndividualCustomerRepository.cs
@@ -1,3 +1,6 @@
+using BankCreditSystem.Core.Repositories;
+using BankCreditSystem.Domain.Entities;
+
 namespace BankCreditSystem.Application.Services.Repositories;
 
 public interface IIndividualCustomerRepository : IAsyncRepository<IndividualCustomer, Guid>
